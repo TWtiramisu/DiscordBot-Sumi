05656969662c297c89e3650b8d840005c604b0db
Tiramisu <wquyya@gmail.com>
2026-08-09 22:30:13 +0800

    Decoupled music file

--- cmds/music.py
+++ cmds/music.py
@@ -1,38 +1,16 @@
 import discord
 from discord.ext import commands
 
-import yt_dlp
+import importlib
 import asyncio
 from config import GLOBAL_COGS
 
-YTDL_OPTIONS = {
-    'format': 'ba/ba*/bestaudio/best', # 優先選擇純音訊軌
-    'extract_flat': 'in_playlist',     # 快速載入清單
-    'nocheckcertificate': True,
-    'ignoreerrors': True,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0',
-    'http_headers': {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, Gecko) Chrome/115.0.0.0 Safari/537.36',
-        'Referer': 'https://www.bilibili.com/'
-    }
-}
+# utils hot reload
+import utils.musics
+importlib.reload(utils.musics)
+from utils.musics import ytdl, FFMPEG_OPTIONS
 
-FFMPEG_OPTIONS = {
-    'before_options': (
-        '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 '
-        '-probesize 10000000 -analyzeduration 10000000 '
-        '-headers "Referer: https://www.bilibili.com/\r\n'
-        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, Gecko) Chrome/115.0.0.0 Safari/537.36\r\n"'
-    ),
-    # -ar 48000 (固定 48kHz, 解決加速問題), -ac 2 (雙聲道)
-    'options': '-vn -ar 48000 -ac 2'
-}
 
-ytdl = yt_dlp.YoutubeDL(YTDL_OPTIONS)
 
 class MusicBot(commands.Cog):
     def __init__(self, bot):
